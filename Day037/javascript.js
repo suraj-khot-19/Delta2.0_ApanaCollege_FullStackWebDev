0874655f8b812ad9c1457825108046d0f08cd051
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-09-25 11:55:26 +0530

    Day37 js vs ts

---
+++ Day037/javascript.js
@@ -0,0 +1,17 @@
+a = 10;
+console.log("a ", a);
+console.log(typeof a);
+
+a = true;
+console.log("a ", a)
+console.log(typeof a);
+
+/*
+PS D:\Workspace\Delta-2.0\Day037> node javascript.js
+a  10
+number
+a  true
+boolean
+*/
+
+//! so javascript is dynamically typed language means we can change the type of variable 
---
+++ Day037/typescript.js
@@ -0,0 +1,3 @@
+
+//! so javascript is dynamically typed language means we can change the type of variable
+//! so typescript is statically typed language means we cannot change the type of variable after it initalized
